f408b64f11f60843e323470c3b37d470ab6ee391
Dmitry Korolev <dmitry@korolev.dk>
2018-07-24 10:19:10 +0300

    feat(extra): add empty

--- src/observable/__tests__/empty.ts
+++ src/observable/__tests__/empty.ts
@@ -1,18 +1,33 @@
 import { commonTest } from '../../helpers/testHelpers/commonTest'
 import { getTestObserver } from '../../helpers/testHelpers/getTestObserver'
-import { EMPTY } from '../empty'
+import { empty, EMPTY } from '../empty'
 
 describe('(Extra) empty', () => {
-  commonTest(EMPTY)
+  commonTest(EMPTY, empty())
 
-  it('should return empty observable', () => {
-    const o = EMPTY
-    const observer = getTestObserver()
+  describe('EMPTY', () => {
+    it('should return empty observable', () => {
+      const o = EMPTY
+      const observer = getTestObserver()
 
-    o.subscribe(observer)
+      o.subscribe(observer)
 
-    expect(observer.next.mock.calls.length).toEqual(0)
-    expect(observer.error.mock.calls.length).toEqual(0)
-    expect(observer.complete.mock.calls.length).toEqual(1)
+      expect(observer.next.mock.calls.length).toEqual(0)
+      expect(observer.error.mock.calls.length).toEqual(0)
+      expect(observer.complete.mock.calls.length).toEqual(1)
+    })
+  })
+
+  describe('empty', () => {
+    it('should return empty observable', () => {
+      const o = empty()
+      const observer = getTestObserver()
+
+      o.subscribe(observer)
+
+      expect(observer.next.mock.calls.length).toEqual(0)
+      expect(observer.error.mock.calls.length).toEqual(0)
+      expect(observer.complete.mock.calls.length).toEqual(1)
+    })
   })
 })
--- src/observable/empty.ts
+++ src/observable/empty.ts
@@ -1,3 +1,5 @@
 import { Observable } from '../core/Observable'
 
 export const EMPTY = new Observable<any>((observer) => observer.complete())
+
+export const empty = () => EMPTY
